2ffc61f9f016301b874ca199d796e954469ee8d7
Blazej Jastrzebski <jastrzebskib@gmail.com>
2017-09-24 23:29:53 +0200

    Move LED business logic to simple views.

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 
 from accounts import views as account_views
+from template import views as template_views
 
 
 urlpatterns = [
@@ -14,4 +15,6 @@ urlpatterns = format_suffix_patterns(urlpatterns)
 urlpatterns += [
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
+    url(r'turn_on/', template_views.turn_on, name='turn_on'),
+    url(r'turn_off/', template_views.turn_off, name='turn_on'),
 ]
--- template/views.py
+++ template/views.py
@@ -1,1 +1,30 @@
-from rest_framework import generics, permissions
+import RPi.GPIO as GPIO
+
+from django.shortcuts import render
+
+from gpio_pins import GPIO_12, GPIO_25, GPIO_24, GPIO_23, GPIO_18
+
+pins = (GPIO_12, GPIO_25, GPIO_24, GPIO_23, GPIO_18)
+
+# helper
+def set_up():
+    GPIO.setmode(GPIO.BOARD)
+    for pin in pins:
+        GPIO.setup(pin, GPIO.OUT)
+
+# helper
+def tear_down(request):
+    GPIO.cleanup()
+
+
+def turn_on(request):
+    set_up()
+    for pin in pins:
+        GPIO.output(pin, GPIO.HIGH)
+
+
+def turn_off(request):
+    set_up()
+    for pin in pins:
+        GPIO.output(pin, GPIO.LOW)
+    tear_down()
